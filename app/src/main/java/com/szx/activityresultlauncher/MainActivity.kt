24de2be2035c43ff94046848cc76a8d964169d63
张玄烨 <zhangxuanye@lehui.com>
2022-04-13 16:16:43 +0800

    完善demo

--- app/src/main/java/com/szx/activityresultlauncher/MainActivity.kt
+++ app/src/main/java/com/szx/activityresultlauncher/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.szx.activityresultlauncher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
 import com.dylanc.activityresult.launcher.StartActivityLauncher
 
 class MainActivity : AppCompatActivity() {
@@ -9,12 +14,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.tvTag).text = "MainActivity"
 
-        startActivityLauncher.launch(intent) { it ->
-            if (it.resultCode == 1) {
-                val result = it.data?.getBooleanExtra("result", false)?:false
-                if (result) {
+        val intent = Intent(this, OtherActivity::class.java)
+        intent.putExtra("data", "main to other")
 
+        findViewById<Button>(R.id.btnJump).setOnClickListener {
+            startActivityLauncher.launch(intent) { it ->
+                if (it.resultCode == 1) {
+                    val result = it.data?.getStringExtra("result") ?: ""
+                    Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
                 }
             }
         }
--- app/src/main/java/com/szx/activityresultlauncher/OtherActivity.kt
+++ app/src/main/java/com/szx/activityresultlauncher/OtherActivity.kt
@@ -1,6 +1,10 @@
 package com.szx.activityresultlauncher
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -9,9 +13,18 @@ import androidx.appcompat.app.AppCompatActivity
  * Desc:
  */
 class OtherActivity : AppCompatActivity() {
+    private var extraValue = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.tvTag).text = "OtherActivity"
 
+        intent.getStringExtra("data").also { extraValue = it.orEmpty() }
+        Toast.makeText(this, extraValue, Toast.LENGTH_SHORT).show()
+
+        findViewById<Button>(R.id.btnJump).setOnClickListener {
+            setResult(1, Intent().putExtra("result", "other to main"))
+            finish()
+        }
     }
 }
